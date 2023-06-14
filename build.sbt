3e48150ac3d3a63e494638269af8d1f54c2832a7
WANGHAITAO-FPGA <18602132366@163.com>
2023-06-14 14:47:37 +0800

    sss

--- build.sbt
+++ build.sbt
@@ -10,7 +10,8 @@ libraryDependencies ++= Seq(
   "com.github.spinalhdl" % "spinalhdl-core_2.12" % spinalVersion,
   "com.github.spinalhdl" % "spinalhdl-lib_2.12" % spinalVersion,
   compilerPlugin("com.github.spinalhdl" % "spinalhdl-idsl-plugin_2.12" % spinalVersion),
-  "org.scalatest" %% "scalatest" % "3.0.8" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.8" % "test",
+  "com.github.wookietreiber" %% "scala-chart" % "latest.integration"
 )
 
 fork := true
---
+++ src/main/scala/TEST/Bin_Test.scala
@@ -0,0 +1,25 @@
+package TEST
+
+import java.io._
+import scalax.chart.api._
+
+object Bin_Test {
+  def main(args: Array[String]): Unit = {
+    val file = new File("D:/data.bin")
+    val inputStream = new FileInputStream(file)
+    val bytes = Stream.continually(inputStream.read).take(200*4).map(_.toByte).toArray
+    val floats = bytes.grouped(4).map(_.reverse).map(java.nio.ByteBuffer.wrap(_).getFloat).toArray
+    val writer = new PrintWriter(new File("output.txt"))
+    for (f <- floats) {
+      writer.write(f.toString + "\n")
+    }
+    writer.close()
+
+    val data = io.Source.fromFile("output.txt").getLines().map(_.toDouble).toArray
+    val chart = XYLineChart(data.zipWithIndex.map{case (y, x) => (x.toDouble, y)}.toSeq)
+    chart.title = "Data Plot"
+    chart.show()
+  }
+}
+
+
